6811b433afff9c01a54d8d6fc9c51c770f72bfe3
MarvinZ <marvinzzz@gmail.com>
2023-05-05 14:59:07 -0600

    httpsnippet work

---
+++ src/app/httpsnippet/httpsnippet.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-httpsnippet',
+  templateUrl: './httpsnippet.component.html',
+  styleUrls: ['./httpsnippet.component.css'],
+})
+export class HttpsnippetComponent {}
